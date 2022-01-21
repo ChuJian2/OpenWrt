44a58350d959df361f856b812a689cfd0edce2da
ChuJian2 <2474681296@qq.com>
2022-01-21 17:49:31 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 #取消bootstrap默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #修改机器名称
 #sed -i 's/OpenWrt/OpenWrt-x64/g' package/base-files/files/bin/config_generate
 #修改磁盘大小为32128K
