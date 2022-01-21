90f4e741c89e68ab8a542d6af2548f38651b021f
ChuJian2 <2474681296@qq.com>
2022-01-21 21:53:32 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 取消bootstrap默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-#default_theme='argon'
-#sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # 修改机器名称
 #sed -i 's/OpenWrt/OpenWrt-x64/g' package/base-files/files/bin/config_generate
